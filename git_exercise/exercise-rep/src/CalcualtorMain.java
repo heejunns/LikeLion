0d511a02257d6ce1bf2d4ff04658fe045fd6b8a4
heejunns <wns2252@hanmail.net>
2022-10-05 10:25:06 +0900

    커밋

---
+++ git_exercise/exercise-rep/src/CalcualtorMain.java
@@ -0,0 +1,10 @@
+public class CalcualtorMain {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        cal.plus(20,10);
+        cal.minus(20,10);
+        cal.multiple(20,10);
+        cal.divid(20,10);
+    }
+
+}
---
+++ git_exercise/exercise-rep/src/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    public void plus(int a,int b){
+        System.out.println(a+b);
+    }
+    public void minus(int a,int b){
+        System.out.println(a-b);
+    }
+    public void multiple(int a,int b) {
+        System.out.println(a * b);
+    }
+    public void divid(int a,int b){
+        System.out.println(a/b);
+    }
+}
