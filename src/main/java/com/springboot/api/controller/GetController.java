04aecd6af445f8cc5d2de57061a87348c28bc480
heejunns <wns2252@hanmail.net>
2022-10-28 03:38:51 +0900

    GET API PathVariable, RequwstParam 활용 구현

--- src/main/java/com/springboot/api/controller/GetController.java
+++ src/main/java/com/springboot/api/controller/GetController.java
@@ -1,16 +1,30 @@
 package com.springboot.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
+
 public class GetController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-
-    public  String getHello(){
+    public String getHello(){
         return "Hello World";
     }
+    @GetMapping(value = "/name")
+    public String getName(){
+        return "hello";
+    }
+
+    // http://localhost:8080/api/v1/get-api/variable1/{String 값}
+    @GetMapping(value = "/variable1/{variable}")
+    public  String getVariable(@PathVariable String variable){
+        return variable;
+    }
+
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(@RequestParam String name, @RequestParam String email,@RequestParam String organization ){
+        return name + " " + email +" "+ organization;
+    }
 }
+
