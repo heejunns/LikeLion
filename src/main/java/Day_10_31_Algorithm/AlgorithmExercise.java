7397aee68150d0ebdabae9196785e4e07bfb4fe6
heejunns <wns2252@hanmail.net>
2022-10-31 11:04:21 +0900

    일단 완성이긴한데..

--- src/main/java/Day_10_31_Algorithm/AlgorithmExercise.java
+++ src/main/java/Day_10_31_Algorithm/AlgorithmExercise.java
@@ -6,31 +6,46 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class AlgorithmExercise {
-    class int[] solution(int[] answers){
-        int[] answer = {};
-        int[] x1 = {1,2,3,4,5};
-        int[] x2 = {2,1,2,3,2,4,2,5};
-        int[] x3 = {3,3,1,1,2,2,4,4,5,5};
-        int cnt1 =0;
-        int cnt2 =0;
-        int cnt3 =0;
-        int max = 0;
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for( int i=0; i< answers.length; i++){
-            if ( answers[i] == x1[ i % 5 ]) {
+
+    public ArrayList<Integer> solution(int[] answers) {
+       // int[] answer = {};
+        int[] x1 = {1, 2, 3, 4, 5};
+        int[] x2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] x3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int cnt3 = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == x1[i % 5]) {
                 cnt1++;
             }
-            if ( answers[i] == x2[ i % 8 ]) {
+            if (answers[i] == x2[i % 8]) {
                 cnt2++;
             }
-            if ( answers[i] == x3[ i % 10 ]) {
-                    cnt3++;
+            if (answers[i] == x3[i % 10]) {
+                cnt3++;
+            }
         }
-            list.add(cnt1);
-            list.add(cnt2);
-            list.add(cnt3);
-            list.sort(Comparator.reverseOrder());
-            // 일단 최대 맞힌 문제 수 찾기
+        list.add(cnt1);
+        list.add(cnt2);
+        list.add(cnt3);
+        list.sort(Comparator.reverseOrder());
+        int max = list.get(0);
 
-        return answer;
+        for (int l = 0; l < list.size(); l++) {
+            if (list.get(l) == max) {
+                result.add(l+1);
+            }
+            }
+        return result;
+        }
+
+
+    public static void main(String[] args) {
+        AlgorithmExercise algorithmExercise = new AlgorithmExercise();
+        int[] answers = {1,3,2,4,2};
+        System.out.println(algorithmExercise.solution(answers));
+    }
 }
